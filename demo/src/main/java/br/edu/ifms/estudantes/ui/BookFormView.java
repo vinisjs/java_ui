a0c05e3ac734f56a801538fdc5dc34da2eb56b38
Pedro <pedroesnarriaga@gmail.com>
2025-01-22 10:32:59 -0300

    screen 1

--- demo/src/main/java/br/edu/ifms/estudantes/ui/BookFormView.java
+++ demo/src/main/java/br/edu/ifms/estudantes/ui/BookFormView.java
@@ -5,6 +5,13 @@ import javax.swing.*;
 public class BookFormView extends JFrame{
     private JPanel Screen1;
     private JTextField textField1;
+    private JTextField textField2;
+    private JPanel CampoTema;
+    private JPanel CampoTitulo;
+    private JPanel CampoAutor;
+    private JTextField textField3;
+    private JPanel CampoISBN;
+    private JTextField textField4;
 
     public BookFormView() {
         this.setTitle("Cadastro de Livros");
