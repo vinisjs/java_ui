bd5b106de3893854db674a17a049239de560cb09
Pedro <pedroesnarriaga@gmail.com>
2025-01-26 10:51:24 -0300

    Estilizando a splash Screen

--- demo/src/main/java/br/edu/ifms/estudantes/ui/SplashScreen.java
+++ demo/src/main/java/br/edu/ifms/estudantes/ui/SplashScreen.java
@@ -1,6 +1,7 @@
 package br.edu.ifms.estudantes.ui;
 
 import javax.swing.*;
+import javax.swing.plaf.basic.BasicButtonUI;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -15,14 +16,45 @@ public class SplashScreen extends JFrame{
         this.setSize(600, 450);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
-        continuarButton.setPreferredSize(new Dimension(200, 30));
-        continuarButton.setFocusPainted(false);
+
+        styleButton(continuarButton);
         this.setVisible(true);
         continuarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 new MenuView(SplashScreen);
                 dispose();
+            }
+        });
+    }
+
+    private  void styleButton(JButton button) {
+        button.setPreferredSize(new Dimension(200, 30));
+        button.setBackground(new Color(2, 133, 199));
+        button.setFocusPainted(false);
+        button.setForeground(Color.WHITE);
+        button.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        button.setUI(new BasicButtonUI() {
+            @Override
+            public void installUI(JComponent c) {
+                super.installUI(c);
+                AbstractButton button = (AbstractButton) c;
+                button.setBorderPainted(false);
+                button.setContentAreaFilled(false);
+                button.setOpaque(false);
+            }
+
+            @Override
+            public void paint(Graphics g, JComponent c) {
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                JButton b = (JButton) c;
+
+                g2.setColor(b.getBackground());
+                g2.fillRoundRect(0, 0, b.getWidth(), b.getHeight(), 20, 20);
+
+                super.paint(g, c);
             }
         });
     }
