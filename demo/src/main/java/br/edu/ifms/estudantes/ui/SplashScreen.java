07a5c91bac79d267ff110ae5c07868ee4c3fafc9
Pedro <pedroesnarriaga@gmail.com>
2025-02-03 10:51:15 -0300

    fix: Separating styles

--- demo/src/main/java/br/edu/ifms/estudantes/ui/SplashScreen.java
+++ demo/src/main/java/br/edu/ifms/estudantes/ui/SplashScreen.java
@@ -1,5 +1,8 @@
 package br.edu.ifms.estudantes.ui;
 
+import br.edu.ifms.estudantes.util.Styles;
+import br.edu.ifms.estudantes.util.Utils;
+
 import javax.swing.*;
 import javax.swing.plaf.basic.BasicButtonUI;
 import java.awt.*;
@@ -10,6 +13,8 @@ public class SplashScreen extends JFrame{
     private JPanel SplashScreen;
     private JButton continuarButton;
 
+    public Styles styles = new Styles();
+
     public SplashScreen() {
         setTitle("início");
         setContentPane(SplashScreen);
@@ -17,44 +22,13 @@ public class SplashScreen extends JFrame{
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
 
-        styleButton(continuarButton);
+        styles.styleButtonSplash(continuarButton);
         this.setVisible(true);
         continuarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 new MenuView(SplashScreen);
                 dispose();
-            }
-        });
-    }
-
-    private  void styleButton(JButton button) {
-        button.setPreferredSize(new Dimension(200, 30));
-        button.setBackground(new Color(2, 133, 199));
-        button.setFocusPainted(false);
-        button.setForeground(Color.WHITE);
-        button.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-
-        button.setUI(new BasicButtonUI() {
-            @Override
-            public void installUI(JComponent c) {
-                super.installUI(c);
-                AbstractButton button = (AbstractButton) c;
-                button.setBorderPainted(false);
-                button.setContentAreaFilled(false);
-                button.setOpaque(false);
-            }
-
-            @Override
-            public void paint(Graphics g, JComponent c) {
-                Graphics2D g2 = (Graphics2D) g;
-                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                JButton b = (JButton) c;
-
-                g2.setColor(b.getBackground());
-                g2.fillRoundRect(0, 0, b.getWidth(), b.getHeight(), 20, 20);
-
-                super.paint(g, c);
             }
         });
     }
--- demo/src/main/java/br/edu/ifms/estudantes/util/Styles.java
+++ demo/src/main/java/br/edu/ifms/estudantes/util/Styles.java
@@ -49,6 +49,37 @@ public class Styles {
         });
     }
 
+    public void styleButtonSplash(JButton button) {
+        button.setPreferredSize(new Dimension(200, 30));
+        button.setBackground(new Color(2, 133, 199));
+        button.setFocusPainted(false);
+        button.setForeground(Color.WHITE);
+        button.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+
+        button.setUI(new BasicButtonUI() {
+            @Override
+            public void installUI(JComponent c) {
+                super.installUI(c);
+                AbstractButton button = (AbstractButton) c;
+                button.setBorderPainted(false);
+                button.setContentAreaFilled(false);
+                button.setOpaque(false);
+            }
+
+            @Override
+            public void paint(Graphics g, JComponent c) {
+                Graphics2D g2 = (Graphics2D) g;
+                g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+                JButton b = (JButton) c;
+
+                g2.setColor(b.getBackground());
+                g2.fillRoundRect(0, 0, b.getWidth(), b.getHeight(), 20, 20);
+
+                super.paint(g, c);
+            }
+        });
+    }
+
     public void alignFields(JPanel panel, String labelText, JComponent component) {
         panel.setLayout(new BorderLayout());
         JLabel label = new JLabel(labelText);
